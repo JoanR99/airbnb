a57b874db24c3d9c01dbf05d3417a81d3df81c2b
Joan Romero <romerojoan1999@gmail.com>
2024-04-21 17:59:39 -0400

    feat: :sparkles: Add navbar

---
+++ src/app/components/navbar/Logo.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+export default function Logo() {
+	const router = useRouter();
+	return (
+		<Image
+			src="/images/airbnb-logo.png"
+			alt="Logo"
+			width={100}
+			height={100}
+			className="hidden md:block cursor-pointer"
+		/>
+	);
+}
